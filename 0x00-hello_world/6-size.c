51385f5bb658f9c4464e8e2547af8b4e773805cc
Oktumas <104770156+Oktumas@users.noreply.github.com>
2022-06-02 16:11:25 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,4 @@
 #include <stdio.h>
-
-
-
 /**
  *
  *  * main - print the size of various types
@@ -17,14 +14,10 @@
  *       * Return: Allways 0 (Success)
  *
  *        */
-
-
-
 int main(void)
 
 {
-
-		printf("Size of a char: %d byte(s)\n", (int) sizeof(char));
+                 printf("Size of a char: %d byte(s)\n", (int) sizeof(char));
 
 			printf("Size of an int: %d byte(s)\n", (int) sizeof(int));
 
