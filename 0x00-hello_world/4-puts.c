317956604693cfc9353e3e3077215ef87759f966
Oktumas <gereddumas@gmail.com>
2022-06-02 07:31:38 -0700

    Hello programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,27 @@
+nclure <stdio.h>
+
+
+
+/**
+ *
+ *  * principal - Point d’entrée
+ *
+ *   *
+ *
+ *    * Description: imprime « La programmation est comme la construction d’un puzzle multilingue »
+ *
+ *     *
+ *
+ *      * Retour: Toujours 0 (Succès)
+ *
+ *       */
+
+int main(void)
+
+{
+
+		puts(« \"La programmation, c’est comme construire un puzzle multilingue »);
+
+			retour (0);
+
+}
