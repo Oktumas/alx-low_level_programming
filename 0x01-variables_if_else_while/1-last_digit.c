09666a55aa188fa1e824de1db7da7cf23f20a176
Oktumas <104770156+Oktumas@users.noreply.github.com>
2022-06-03 16:09:38 +0100

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,4 +1,4 @@
-nclude <stdlib.h>
+#include <stdlib.h>
 <<<<<<< HEAD
 
 =======
@@ -7,63 +7,63 @@ nclude <stdlib.h>
 #include <stdio.h>
 
 /**
- * <<<<<<< HEAD
- *  *
- *   * * main-print the last digit of the random number
- *    *
- *     * * stored in the variable n
- *      *
- *       * * Return: 0 if successful
- *        *
- *         * */
+<<<<<<< HEAD
+ *
+ * * main-print the last digit of the random number
+ *
+ * * stored in the variable n
+ *
+ * * Return: 0 if successful
+ *
+ * */
 
 =======
 * main-print the last digit of the random number
-* * stored in the variable n
-* * Return: 0 if successful
-* */
+* stored in the variable n
+* Return: 0 if successful
+*/
 >>>>>>> e3c48d54dca0af5d1da7d7d43d87cc3e4280d57c
 int main(void)
 {
-	<<<<<<< HEAD
+<<<<<<< HEAD
 
-			int n;
+	int n;
 
 
 
-		srand(time(0));
+	srand(time(0));
 
-			n = rand() - RAND_MAX / 2;
+	n = rand() - RAND_MAX / 2;
 
-				printf("Last digit of %d is %d ", n, n % 10);
+	printf("Last digit of %d is %d ", n, n % 10);
 
-					if (n % 10 > 5)
+	if (n % 10 > 5)
 
-								printf("and is greater than 5\n");
+		printf("and is greater than 5\n");
 
-						else if (n % 10 == 0)
+	else if (n % 10 == 0)
 
-									printf("and is 0\n");
+		printf("and is 0\n");
 
-							else if (n % 10 < 6 && n % 10 != 0)
+	else if (n % 10 < 6 && n % 10 != 0)
 
-										printf("and is less than 6 and not 0\n");
+		printf("and is less than 6 and not 0\n");
 
-								return (0);
+	return (0);
 
-								=======
-									int n;
+=======
+int n;
 
-								srand(time(0));
-								n = rand() - RAND_MAX / 2;
-								printf("Last digit of %d is %d ", n, n % 10);
-								if (n % 10 > 5)
-									printf("and is greater than 5\n");
-								else if (n % 10 == 0)
-									printf("and is 0\n");
-								else if (n % 10 < 6 && n % 10 != 0)
-									printf("and is less than 6 and not 0\n");
-								return (0);
-								>>>>>>> e3c48d54dca0af5d1da7d7d43d87cc3e4280d57c
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+printf("Last digit of %d is %d ", n, n % 10);
+if (n % 10 > 5)
+printf("and is greater than 5\n");
+else if (n % 10 == 0)
+printf("and is 0\n");
+else if (n % 10 < 6 && n % 10 != 0)
+printf("and is less than 6 and not 0\n");
+return (0);
+>>>>>>> e3c48d54dca0af5d1da7d7d43d87cc3e4280d57c
 }
 
