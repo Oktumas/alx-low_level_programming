134856b0b791bbbf627d09272d10002980e62f64
Oktumas <104770156+Oktumas@users.noreply.github.com>
2022-06-03 17:26:19 +0100

    Update 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,38 @@
+# inclure  < stdio.h >
+
+/* *
+
+* main - imprime les alphabets en minuscules et majuscules,
+
+* suivi d'une nouvelle ligne
+
+* Retour : Toujours 0 (Succès)
+
+ */
+
+int  principal ( vide )
+
+{
+
+int je ;
+
+pour (je = ' z ' ; je >= ' une ' ; je--)
+
+{
+
+putchar (i);
+
+}
+
+putchar ( ' \n ' );
+
+retour ( 0 );
+
+}
+
+© 2022 GitHub, Inc.
+
+Conditions
+
+Intimité
+
