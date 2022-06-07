79346b118088894dd98339d00b174f3a758c9867
Oktumas <gereddumas@gmail.com>
2022-06-07 06:16:35 -0700

    Functions nested loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,39 @@
+lude "main.h"
+
+
+
+/**
+ *
+ *  * _abs - computes the absolute value of an integer
+ *
+ *   * @n: n is an integer
+ *
+ *    *
+ *
+ *     * Return: integer
+ *
+ *      */
+
+
+
+int _abs(int n)
+
+{
+
+		if (n >= 0)
+
+				{
+
+							return (n);
+
+								}
+
+			else
+
+					{
+
+								return (-n);
+
+									}
+
+}
