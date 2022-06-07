0cd76c88419b562e8afec5eaadddaca7de9dce04
Oktumas <104770156+Oktumas@users.noreply.github.com>
2022-06-07 13:51:41 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,20 @@
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
+#include "main.h"
+#include <string.h>
+
+/**
+* main - Prints numbers between 00 to 89.
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char word[9] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 8; i++)
+		_putchar(word[i]);
+
+	_putchar('\n');
+
+	return (0);
+}
