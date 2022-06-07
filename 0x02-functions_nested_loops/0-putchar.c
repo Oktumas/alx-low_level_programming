94c78c4a46ac32bbfdbca12a9023be393bf9fbfe
Oktumas <104770156+Oktumas@users.noreply.github.com>
2022-06-07 12:40:42 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,3 +1,4 @@
+
 #include "main.h"
 
 
@@ -9,12 +10,14 @@
 
 int main(void)
 {
-_putchar('m');
-_putchar('a');
-_putchar('i');
-_putchar('n');
-_putchar('.');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
 _putchar('h');
+_putchar('a');
+_putchar('r');
 _putchar('\n');
 
 return (0);
