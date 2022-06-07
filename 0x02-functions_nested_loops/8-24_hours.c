4600d454af99d1d96af0e13ecda4322f13a955a8
Oktumas <104770156+Oktumas@users.noreply.github.com>
2022-06-07 14:44:34 +0100

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,41 +1,24 @@
-ck_bauer - Check Holberton
+#include "main.h"
 
+/**
+ * jack_bauer - Check Holberton
  * Description: function prints every minute in one day
- *
- *  * Return: Nothing
- *
- *   */
-
- void jack_bauer(void)
-
+ * Return: Nothing
+ */
+void jack_bauer(void)
 {
+	int H, M;
 
-		int H, M;
-
-
-
-			for (H = 0; H < 24; H++)
-
-					{
-
-								for (M = 0; M < 60; M++)
-
-											{
-
-															_putchar((H / 10) + '0');
-
-																		_putchar((H % 10) + '0');
-
-																					_putchar(':');
-
-																								_putchar((M / 10) + '0');
-
-																											_putchar((M % 10) + '0');
-
-																														_putchar('\n');
-
-																																}
-
-									}
-
+	for (H = 0; H < 24; H++)
+	{
+		for (M = 0; M < 60; M++)
+		{
+			_putchar((H / 10) + '0');
+			_putchar((H % 10) + '0');
+			_putchar(':');
+			_putchar((M / 10) + '0');
+			_putchar((M % 10) + '0');
+			_putchar('\n');
+		}
+	}
 }
