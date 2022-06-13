f946be2f2753830ec039db1d1dae833c5eeec375
Oktumas <104770156+Oktumas@users.noreply.github.com>
2022-06-13 15:00:01 +0100

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,19 +1,10 @@
-nclude "main.h"
-
+#include "main.h"
 /**
- *
- *  * reset_to_98 - update and interger value to 98
- *
- *   * @n: A pointer to an int that will be updated
- *
- *    * Return: 0
- *
- *     */
-
+ * reset_to_98 - update and interger value to 98
+ * @n: A pointer to an int that will be updated
+ * Return: 0
+ */
 void reset_to_98(int *n)
-
 {
-
-	*n = 98;
-
+*n = 98;
 }
