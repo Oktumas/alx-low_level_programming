c428de8e82b6a3956502f0cf2e53e45e7b77b840
Oktumas <gereddumas@gmail.com>
2022-06-13 06:56:58 -0700

    Pointers arrays strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,19 @@
+nclude "main.h"
+
+/**
+ *
+ *  * reset_to_98 - update and interger value to 98
+ *
+ *   * @n: A pointer to an int that will be updated
+ *
+ *    * Return: 0
+ *
+ *     */
+
+void reset_to_98(int *n)
+
+{
+
+	*n = 98;
+
+}
