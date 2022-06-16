8e10574e0d948c0dcdf15561aef2d4604e09b25e
Oktumas <104770156+Oktumas@users.noreply.github.com>
2022-06-16 20:30:27 +0100

    Update 104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,40 +2,54 @@
 #include <stdio.h>
 
 /**
- *print_buffer-Print a buffer 10 bytes per line.
- *@b: Buffer address.
- *@size: Number of characters to be printed.
- *Return: no return.
- */
-void print_buffer(char *b, int size)
+* print_buffer - Print a buffer 10 bytes per line.
+* @b: Buffer address.
+* @size: Number of characters to be printed.
+*/
+
+void  print_buffer(char *b, int size)
 {
 int j, k, l;
-
 if (size <= 0)
+{
 printf("\n");
+}
 else
 {
 for (j = 0; j < size; j += 10)
 {
-printf("%.8x:", j);
+printf("%.8x:", j); 
 for (k = j; k < j + 10; k++)
 {
-if (k % 2 == 0)	
-printf("  ");	
+if (k % 2 == 0)
+{
+printf(" ");
 if (k < size)
+{
 printf("%.2x", *(b + k));
+}
 else
-printf("  ");
+{
+printf("%c", *(b + l));
+}
 }
 printf(" ");
 for (l = j; l < j + 10; l++)
 {
-if (l >= size)
+if (1 >= size)
+{
 break;
-if (*(b + l) < 32 || *(b + l) > 126)
-printf("%c", '.');
+}
+if (*(b + 1)  < 32 || *(b + l) > 126)
+{
+printf("%c", ',');
+}
 else
+{
 printf("%c", *(b + l));
+}
+}
+printf("%c",, *(b + l));
 }
 printf("\n");
 }
