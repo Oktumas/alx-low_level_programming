d048f26c33a8448f8987b83abc92a92120a3df28
Oktumas <104770156+Oktumas@users.noreply.github.com>
2022-06-16 19:08:39 +0100

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,47 +1,24 @@
-nclude "main.h"
-
+#include "main.h"
 /**
+ * _strncpy - write a functino that two strings. it will us
+ * @dest: Second string
+ * @src: first string
+ * @n: this is the number bytes fro src
  *
- *  * _strncpy - write a functino that two strings. it will us
- *
- *   * @dest: Second string
- *
- *    * @src: first string
- *
- *     * @n: this is the number bytes fro src
- *
- *      *
- *
- *       * Return: My return
- *
- *        */
-
-
+ * Return: My return
+ */
 
 char *_strncpy(char *dest, char *src, int n)
-
 {
+int i;
 
-	int i;
-
-
-
-	for (i = 0; i < n && src[i] != '\0' ; i++)
-
-	{
-
-		dest[i] = src[i];
-
-	}
-
-	for (; i < n; i++)
-
-	{
-
-		dest[i] = '\0';
-
-	}
-
-	return (dest);
-
+for (i = 0; i < n && src[i] != '\0' ; i++)
+{
+dest[i] = src[i];
+}
+for (; i < n; i++)
+{
+dest[i] = '\0';
+}
+return (dest);
 }
