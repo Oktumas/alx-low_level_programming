3763df7534b5b04283b713c0688a0efbc55dbfc2
Oktumas <104770156+Oktumas@users.noreply.github.com>
2022-06-16 19:34:59 +0100

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,53 +1,21 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
- * reverse_array -
- * @a: Second string
- * @n: first string
- *
- * Return: My return
+ *reverse_array - reverse an array.
+ *@a: array.
+ *@n: array's length.
  */
-void print_array(int *a, int n)
-{
-    int i;
-
-    i = 0;
-    while (i < n)
-    {
-        if (i != 0)
-        {
-            printf(", ");
-        }
-        printf("%d", a[i]);
-        i++;
-    }
-    printf("\n");
-}
 
 void reverse_array(int *a, int n)
 {
-    int i;
-    int j = n;
+int i, j, tmp;
 
-    int *aCopy;
-
-    for (i = 0; i < n; i++)
-    {
-        j--;
-
-        aCopy[i] = a[j];
-        printf("%d", aCopy[i]);
-    }
-}
-
-int main()
+j = n - 1;
+for (i = 0; i < n / 2; i++)
 {
-    int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
-
-    // print_array(a, sizeof(a) / sizeof(int));
-    reverse_array(a, sizeof(a) / sizeof(int));
-    // print_array(a, sizeof(a) / sizeof(int));
-    return (0);
+tmp = a[i];
+a[i] = a[j];
+a[j] = tmp;
+j--;
+}
 }
