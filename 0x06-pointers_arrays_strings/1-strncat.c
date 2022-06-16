0b053c698f330ca217e7c0e380bcda90954508c0
Oktumas <104770156+Oktumas@users.noreply.github.com>
2022-06-16 19:02:20 +0100

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,49 +1,25 @@
-nclude "main.h"
-
-
+#include "main.h"
 
 /**
+ * _strcat - concatenates two strings
  *
- *  * _strcat - concatenates two strings
+ * @dest: Second string
+ * @src: first string
  *
- *   *
- *
- *    * @dest: Second string
- *
- *     * @src: first string
- *
- *      *
- *
- *       * Return: pointer to resulting string dest
- *
- *        */
-
-
+ * Return: pointer to resulting string dest
+ */
 
 char *_strncat(char *dest, char *src, int n)
-
 {
+int x, y;
 
-	int x, y;
-
-
-
-	for (x = 0; dest[x] != '\0'; x++)
-
-		;
-
-	for (y = 1; y <= n; y++)
-
-	{
-
-		dest[x] = src[y - 1];
-
-		x++;
-
-	}
-
-	dest[x] = '\0';
-
-	return (dest);
-
+for (x = 0; dest[x] != '\0'; x++)
+;
+for (y = 1; y <= n; y++)
+{
+dest[x] = src[y - 1];
+x++;
+}
+dest[x] = '\0';
+return (dest);
 }
