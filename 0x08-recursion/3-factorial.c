6c5cd369c4057ee088cff83d3b5b240ac9f2d5d7
Oktumas <104770156+Oktumas@users.noreply.github.com>
2022-06-22 19:41:44 +0100

    Update 3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,35 +1,18 @@
-nclude "main.h"
-
-
+#include "main.h"
 
 /**
+ * factorial - gets factorial of n
+ * @n: integer
  *
- *  * factorial - gets factorial of n
- *
- *   * @n: integer
- *
- *    *
- *
- *     * Return: 1 if successful
- *
- *      * On error, -1 is returned, and errno is set appropriately.
- *
- *       */
-
-
+ * Return: 1 if successful
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 
 int factorial(int n)
-
 {
-
-	if (n < 0)
-
-		return (-1);
-
-	if (n == 0)
-
-		return (1);
-
-	return (n * factorial(n - 1));
-
+if (n < 0)
+return (-1);
+if (n == 0)
+return (1);
+return (n * factorial(n - 1));
 }
