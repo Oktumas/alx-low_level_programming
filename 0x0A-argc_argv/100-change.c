86c5f077a064765ff9a101afd8c4a167bca3bc32
Oktumas <104770156+Oktumas@users.noreply.github.com>
2022-06-24 17:44:08 +0100

    Update 100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,30 @@
+cd root/
+cd alx-low_level_programming
+mkdir 0x0A-argc_argv
+cd 0x0A-argc_argv
+echo "My level programming A" > README.md
+cat README.md
+touch 0-whatsmyname.c
+chmod u+x 0-whatsmyname.c
+touch 1-args.c
+chmod u+x 1-args.c
+touch 2-args.c
+chmod u+x 2-args.c
+touch 3-mul.c
+chmod u+x 3-mul.c
+touch 4-add.c 
+chmod u+x 4-add.c
+touch 100-change.c 
+chmod u+x 100-change.c
+touch mintty.exe.stackdump
+chmod u+x mintty.exe.stackdump
+git add .
+git commit -m "My level programming A"
+git push
+
+vi 1-args.c
+vi 2-args.c
+vi 3-mul.c
+vi 4-add.c
+vi 100-change.c
+vi mintty.exe.stackdump
