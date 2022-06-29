b5e2c9cb232fc130b9d8d971770d230b8518f7a4
Oktumas <104770156+Oktumas@users.noreply.github.com>
2022-06-29 22:59:51 +0100

    Update 0-isupper.c

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,39 +1,20 @@
-nclude "main.h"
-
+#include "main.h"
 /**
- *
- *  * _isupper-evaluate if letter is uppercase
- *
- *   *@c: print int
- *
- *    * Return: 0 if successful
- *
- *     */
-
-
+ * _isupper-evaluate if letter is uppercase
+ *@c: print int
+ * Return: 0 if successful
+ */
 
 int _isupper(int c)
-
 {
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
 
-	if (c >= 'A' && c <= 'Z')
-
-	{
-
-		return (1);
-
-	}
-
-
-
-	else
-
-	{
-
-		return (0);
-
-	}
-
-
+else
+{
+return (0);
+}
 
 }
