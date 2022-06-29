04a1d924557c12611a42b8e9897b3ac4effdb4f9
Oktumas <gereddumas@gmail.com>
2022-06-29 16:42:22 -0700

    My static libraries programming

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,20 +1,33 @@
-#include "main.h"
+nclude "main.h"
+
 /**
- * _isupper-evaluate if letter is uppercase
- *@c: print int
- * Return: 0 if successful
- */
+ *
+ *  * _isupper - checks if c is uppercase
+ *
+ *   * @c: the var to be evaluated
+ *
+ *    *
+ *
+ *     * Return: 1 for uppercase else 0
+ *
+ *      */
 
 int _isupper(int c)
-{
-if (c >= 'A' && c <= 'Z')
-{
-return (1);
-}
 
-else
 {
-return (0);
-}
+
+
+
+		if (c >= 'A' && c <= 'Z')
+
+				return (1);
+
+
+
+
+
+			else
+
+					return (0);
 
 }
