59261c303fc4318c99d82aeaeea3bfa5bb825b0f
Oktumas <104770156+Oktumas@users.noreply.github.com>
2022-06-30 01:15:38 +0100

    Update 2-strchr.c

--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -1,56 +1,16 @@
-<<<<<<< HEAD
-nclude "main.h"
-
-
-
+#include "main.h"
 /**
- *
- *  * _strchr - locates a character in a string
- *
- *   * @s: string input
- *
- *    * @c: character to find
- *
- *     * Return: pointer to first occurence of c character
- *
- *      */
-
-
-
+ * *_strchr - function that locates a character in a string.
+ * @s: pointer to a string
+ * @c: character to locate
+ * Return: string after occurance of the character
+ */
 char *_strchr(char *s, char c)
-
 {
-
 	unsigned int i;
 
-
-
 	for (i = 0; s[i] != '\0'; i++)
-
 		if (s[i] == c)
-
 			break;
-
 	return (s[i] == c ? (s + i) : '\0');
-
 }
-=======
-#include "main.h"
-
-/**
- * _strchr - locates a character in a string
- * @s: string input
- * @c: character to find
- * Return: pointer to first occurence of c character
- */
-
-char *_strchr(char *s, char c)
-{
-unsigned int i;
-
-for (i = 0; s[i] != '\0'; i++)
-if (s[i] == c)
-break;
-return (s[i] == c ? (s + i) : '\0');
-}
->>>>>>> a6aedc5adac0707b50dd470507d24378ce4f0a02
