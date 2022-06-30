99ca9550f0e6d346077c058bc848de95ab829359
Oktumas <104770156+Oktumas@users.noreply.github.com>
2022-06-30 09:28:14 +0100

    Update main.h

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,21 @@
+cd root/
+cd alx-low_level_programming
+mkdir 0x0C-more_malloc_free
+cd 0x0C-more_malloc_free
+echo "My more malloc free" > README.md
+cat README.md
+touch 0-malloc_checked.c
+chmod u+x 0-malloc_checked.c
+touch 1-string_nconcat.c
+chmod u+x 1-string_nconcat.c
+touch 2-calloc.c
+chmod u+x 2-calloc.c
+touch 3-array_range.c
+chmod u+x 3-array_range.c
+touch 100-realloc.c
+chmod u+x 100-realloc.c
+touch 101-mul.c
+chmod u+x 101-mul.c
+git add .
+git commit -m "My more malloc free programming"
+git push
