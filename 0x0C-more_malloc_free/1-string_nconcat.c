be29528a932bb6aa6e449dc4c45a3b7b31c38c12
Oktumas <104770156+Oktumas@users.noreply.github.com>
2022-06-30 22:16:15 +0100

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,48 +1,46 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
- *string_nconcat - concat 2 string
- *@s1: string 1
- *@s2: string 2
- *@n: integer value
- *Return: pointer points to a new allocated space else NULL
- */
+* string_nconcat - concatenates two strings.
+* @s1: first string
+* @s2: second string
+* @n: index
+* Return: char pointer
+*/
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *str;
-unsigned int s1_len, s2_len, i;
-
+char *p;
+unsigned int size1 = 0, size2 = 0, i;
 
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
 
-for (s1_len = 0; s1[s1_len] != '\0'; s1_len++)
-;
+while (s1[size1] != '\0')
+{
+size1++;
+}
+while (s2[size2] != '\0')
+{
+size2++;
+}
+if (n > size2)
+n = size2;
+p = malloc((size1 + n + 1) * sizeof(char));
 
-for (s2_len = 0; s2[s2_len] != '\0'; s2_len++)
-;
-
-str = malloc(s1_len + s2_len + 1);
-
-
-if (n >= s2_len)
-n = s2_len;
-
-for (i = 0; s1[i] != '\0'; i++)
-str[i] = s1[i];
-
-for (i = 0; i < n; i++)
-str[s1_len + i] = s2[i];
-
-str[s1_len + n] = '\0';
-
-if (str == NULL)
-return (NULL);
-
-return (str);
+if (p == NULL)
+return (0);
+for (i = 0; i < size1; i++)
+{
+p[i] = s1[i];
+}
+for (; i < (size1 + n); i++)
+{
+p[i] = s2[i - size1];
+}
+p[i] = '\0';
+return (p);
 }
