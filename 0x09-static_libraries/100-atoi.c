47525985d5f0826316ecd997a78713ed9979167b
Oktumas <104770156+Oktumas@users.noreply.github.com>
2022-06-30 01:12:37 +0100

    Update 100-atoi.c

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,143 +1,26 @@
-<<<<<<< HEAD
-nclude "main.h"
-
-
-
+#include "main.h"
 /**
- *
- *  * is_numerical - checks if it is a digit
- *
- *   * @n: number
- *
- *    * Return: 1 or 0
- *
- *     */
-
-int is_numerical(unsigned int n)
-
-{
-
-	return (n >= '0' && n <= '9');
-
-}
-
-
-
-/**
- *
- *  * _atoi - covert a string to an integer
- *
- *   * *@s: string
- *
- *    * Return: num
- *
- *     */
-
+ * _atoi - convert a string to an integer
+ * @s: the character pointer
+ * Return: integer
+ */
 int _atoi(char *s)
-
 {
+	int sign = 1, i = 0;
+	unsigned int res = 0;
 
-	unsigned int number, i;
-
-	int sign;
-
-
-
-	sign = 1;
-
-	number = 0;
-
-
-
-
-
-
-
-	for (i = 0; s[i] != '\0'; i++)
-
+	while (!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
 	{
-
-		if (is_numerical(s[i]))
-
-		{
-
-			number = (s[i] - 48) + number * 10;
-
-
-
-			if (s[i + 1] == ' ')
-
-				break;
-
-		}
-
-		else if (s[i] == '-')
-
-		{
-
+		if (s[i] == '-')
 			sign *= -1;
-
-		}
-
-
+		i++;
 
 	}
-
-
-
-	return (number *sign);
-
-
-
-
-
+	while (s[i] <= '9' && (s[i] >= '0' && s[i] != '\0'))
+	{
+		res = (res * 10) + (s[i] - '0');
+		i++;
+	}
+	res *= sign;
+	return (res);
 }
-=======
-#include "main.h"
-
-/**
- * is_numerical - checks if it is a digit
- * @n: number
- * Return: 1 or 0
- */
-int is_numerical(unsigned int n)
-{
-return (n >= '0' && n <= '9');
-}
-
-/**
- * _atoi - covert a string to an integer
- * *@s: string
- * Return: num
- */
-int _atoi(char *s)
-{
-unsigned int number, i;
-int sign;
-
-sign = 1;
-number = 0;
-
-
-
-for (i = 0; s[i] != '\0'; i++)
-{
-if (is_numerical(s[i]))
-{
-number = (s[i] - 48) + number * 10;
-
-if (s[i + 1] == ' ')
-break;
-}
-else if (s[i] == '-')
-{
-sign *= -1;
-}
-
-}
-
-return (number *sign);
-
-
-}
->>>>>>> eec53702fc78c6c34d95a79ca73ba0e58eb6f36b
