813736d0aff1b9fbc16b0013cbd21bae44c98591
Oktumas <104770156+Oktumas@users.noreply.github.com>
2022-07-04 14:29:27 +0100

    Update 0-object_like_macro.h

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,29 @@
+cd root/
+cd alx-low_level_programming
+mkdir 0x0D-preprocessor
+cd 0x0D-preprocessor
+echo "My preprocessor" > README.md
+cat README.md
+touch 0-object_like_macro.h
+chmod u+x 0-object_like_macro.h
+touch 1-pi.h
+chmod u+x 1-pi.h
+touch 2-main.c
+chmod u+x 2-main.c
+touch 3-function_like_macro.h
+chmod u+x 3-function_like_macro.h
+touch 4-sum.h
+chmod u+x 4-sum.h
+git add .
+git comit -m "My preprocessor programming"
+git push
+
+
+
+
+
+vi 0-object_like_macro.h
+vi 1-pi.h
+vi 2-main.c
+vi 3-function_like_macro.h
+vi 4-sum.h
