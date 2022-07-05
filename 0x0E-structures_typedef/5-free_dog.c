d88841ac9bb95f3fe29e57afa5d8992c65b2f48b
Oktumas <104770156+Oktumas@users.noreply.github.com>
2022-07-05 11:02:23 +0100

    Update 5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+cd root/
+cd alx-low_level_programming
+mkdir 0x0E-structures_typedef
+cd 0x0E-structures_typedef
+echo "My structures typedef" > README.md
+cat README.md
+touch dog.h
+chmod u+x dog.h
+touch 1-init_dog.c
+chmod u+x 1-init_dog.c
+touch 2-print_dog.c
+chmod u+x 2-print_dog.c
+touch 4-new_dog.c
+chmod u+x 4-new_dog.c
+touch 5-free_dog.c
+chmod u+x 5-free_dog.c
+git add .
+git commit -m "My structures typedef programming"
+git push
