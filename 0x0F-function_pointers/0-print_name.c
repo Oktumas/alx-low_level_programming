553cff1c351c8e74371af06455fad057d3cf685a
Oktumas <104770156+Oktumas@users.noreply.github.com>
2022-07-06 16:34:26 +0100

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,31 +1,16 @@
-nclure « function_pointers.h »
-
+#inclure « function_pointers.h »
 #inclure <stdio.h>
 
-
-
 /**
- *
- *  *print_name - imprime le nom d’une fonction
- *
- *   *@name : pointeur
- *
- *    *@f : pointeur
- *
- *     *Retour : nul
- *
- *      */
-
-
+ *print_name - imprime le nom d’une fonction
+ *@name : pointeur
+ *@f : pointeur
+ *Retour : nul
+ */
 
 void print_name(char *nom, void (*f)(char *))
-
 {
-
-	if (nom == NULL || f == NULL)
-
-		rendre;
-
-	(*f) (nom);
-
+if (nom == NULL || f == NULL)
+rendre;
+(*f) (nom);
 }
