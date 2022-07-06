f7aa922d631551024245af2d86cde4fea4216cc0
Oktumas <gereddumas@gmail.com>
2022-07-06 08:33:45 -0700

    My function pointers programming

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,31 @@
+nclure « function_pointers.h »
+
+#inclure <stdio.h>
+
+
+
+/**
+ *
+ *  *print_name - imprime le nom d’une fonction
+ *
+ *   *@name : pointeur
+ *
+ *    *@f : pointeur
+ *
+ *     *Retour : nul
+ *
+ *      */
+
+
+
+void print_name(char *nom, void (*f)(char *))
+
+{
+
+	if (nom == NULL || f == NULL)
+
+		rendre;
+
+	(*f) (nom);
+
+}
