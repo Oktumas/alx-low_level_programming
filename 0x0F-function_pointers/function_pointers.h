85eed45628d3e66b3470a208b6bceb6832fdce09
Oktumas <gereddumas@gmail.com>
2022-07-06 08:55:31 -0700

    My function pointers programming

--- 0x0F-function_pointers/function_pointers.h
+++
@@ -1,8 +0,0 @@
-#ifndef _HEADER_
-#définir _HEADER_
-#inclure <stdlib.h>
-
-void print_name(char *nom, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
-int int_index(int *array, int size, int (*cmp)(int));
-#endif
