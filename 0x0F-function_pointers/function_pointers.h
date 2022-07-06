38c4071d09e26b32a4f4e7f4b096fedf022481ca
Oktumas <104770156+Oktumas@users.noreply.github.com>
2022-07-06 16:37:54 +0100

    Update function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,10 +1,8 @@
-#ifndef FUNCTION_POINTERS_H
-#définir FUNCTION_POINTERS_H
+#ifndef _HEADER_
+#définir _HEADER_
+#inclure <stdlib.h>
 
-#inclure <stdio.h>
 void print_name(char *nom, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-int _putchar(char c);
-
 #endif
