b7032eebd33819d364959536e73c08ce99985081
Oktumas <gereddumas@gmail.com>
2022-07-14 16:20:46 -0700

    My singly inked lists programming

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+nclude "lists.h"
+/**
+ *  *free_list - frees memory from list
+ *   *@head: pointer to the list
+ *    *Author: oktumas
+ *     */
+void free_list(list_t *head)
+{
+	list_t *temp = 0;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
+
