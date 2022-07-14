b31544fdc0384ad99fd123ccd85261715c7fbc1c
Oktumas <104770156+Oktumas@users.noreply.github.com>
2022-07-15 00:23:27 +0100

    Update listes.h

--- 0x12-singly_linked_lists/listes.h
+++ 0x12-singly_linked_lists/listes.h
@@ -1,23 +1,23 @@
-fndef LISTS_H
+#ifndef LISTS_H
 #define LISTS_H
 #include <stdio.h>
 #include <string.h>
 #include<stdlib.h>
 
 /**
- *  *struct list_s - singly linked list
- *   *@str: string - placed in memory
- *    *@len: length of the string
- *     *@next: next point in the node
- *      *Author: Oktumas
- *       *Description: singly linked list structure
- *        *for Holberton project
- *         */
+ *struct list_s - singly linked list
+ *@str: string - placed in memory
+ *@len: length of the string
+ *@next: next point in the node
+ *Author: Oktumas
+ *Description: singly linked list structure
+ *for Holberton project
+ */
 typedef struct list_s
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
+char *str;
+unsigned int len;
+struct list_s *next;
 }list_t;
 void liebre_tortuga(void) __attribute__ ((constructor));
 list_t *add_node(list_t **head, const char *str);
@@ -27,4 +27,3 @@ int _strlen(const char *s);
 size_t list_len(const list_t *h);
 size_t print_list(const list_t *h);
 #endif
-
