536d0c0349ee20dcc278531fbb25e59b8fda853a
Oktumas <104770156+Oktumas@users.noreply.github.com>
2022-07-19 17:38:23 +0100

    Update lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,21 +1,21 @@
-STS_H
+#ifndef LISTS_H
 #define LISTS_H
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 /**
- *  * struct listint_s - singly linked list
- *   * @n: integer
- *    * @next: points to the next node
- *     *
- *      * Description: singly linked list node structure
- *       * for Holberton project
- *        */
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
 typedef struct listint_s
 {
-	int n;
-	struct listint_s *next;
+int n;
+struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
@@ -35,4 +35,3 @@ size_t print_listint_safe(const listint_t *head);
 listint_t *find_listint_loop(listint_t *head);
 size_t free_listint_safe(listint_t **h);
 #endif
-
