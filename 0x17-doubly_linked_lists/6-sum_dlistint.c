2a1235c9e05062d573ac718a845288004fd37733
Oktumas <104770156+Oktumas@users.noreply.github.com>
2022-09-02 09:37:50 +0100

    Update 6-sum_dlistint.c

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,21 +1,20 @@
-nclude "lists.h"
+#include "lists.h"
 
 /**
- *  * sum_dlistint - Sums all the data of a dlistint_t list.
- *   * @head: The head of the dlistint_t list.
- *    *
- *     * Return: The sum of all the data.
- *      */
+ * sum_dlistint - Sums all the data of a dlistint_t list.
+ * @head: The head of the dlistint_t list.
+ *
+ * Return: The sum of all the data.
+ */
 int sum_dlistint(dlistint_t *head)
 {
-		int sum = 0;
+	int sum = 0;
 
-			while (head)
-					{
-								sum += head->n;
-										head = head->next;
-											}
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
 
-				return (sum);
+	return (sum);
 }
-
