a9a56d1d493d35f2b56303017c834242bd66b01b
Oktumas <104770156+Oktumas@users.noreply.github.com>
2022-09-02 09:42:34 +0100

    Update 8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,43 +1,42 @@
-nclude "lists.h"
+#include "lists.h"
 
 /**
- *  * delete_dnodeint_at_index - Deletes a node from a dlistint_t
- *   *                            at a given index.
- *    * @head: A pointer to the head of the dlistint_t.
- *     * @index: The index of the node to delete.
- *      *
- *       * Return: Upon success - 1.
- *        *         Otherwise - -1.
- *         */
+ * delete_dnodeint_at_index - Deletes a node from a dlistint_t
+ *                            at a given index.
+ * @head: A pointer to the head of the dlistint_t.
+ * @index: The index of the node to delete.
+ *
+ * Return: Upon success - 1.
+ *         Otherwise - -1.
+ */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-		dlistint_t *tmp = *head;
+	dlistint_t *tmp = *head;
 
-			if (*head == NULL)
-						return (-1);
+	if (*head == NULL)
+		return (-1);
 
-				for (; index != 0; index--)
-						{
-									if (tmp == NULL)
-													return (-1);
-											tmp = tmp->next;
-												}
+	for (; index != 0; index--)
+	{
+		if (tmp == NULL)
+			return (-1);
+		tmp = tmp->next;
+	}
 
-					if (tmp == *head)
-							{
-										*head = tmp->next;
-												if (*head != NULL)
-																(*head)->prev = NULL;
-													}
+	if (tmp == *head)
+	{
+		*head = tmp->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+	}
 
-						else
-								{
-											tmp->prev->next = tmp->next;
-													if (tmp->next != NULL)
-																	tmp->next->prev = tmp->prev;
-														}
+	else
+	{
+		tmp->prev->next = tmp->next;
+		if (tmp->next != NULL)
+			tmp->next->prev = tmp->prev;
+	}
 
-							free(tmp);
-								return (1);
+	free(tmp);
+	return (1);
 }
-
